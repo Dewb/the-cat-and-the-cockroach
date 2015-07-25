20ce4834b2dbe88dc743888054b7821b00ae5fa9
Michael Dewberry <michael.dewberry@gmail>
2015-07-25 19:08:02 -0400

    Prototype serial data display server

---
+++ Display/server/main.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var webport = process.env.PORT || 8000;
+
+var SerialPort = require("serialport").SerialPort
+var arduinoSerial = new SerialPort("/dev/virtual-tty", {
+  baudrate: 57600
+});
+
+
+server.listen(webport, function () {
+  console.log('Server listening at port %d', webport);
+});
+
+app.use('/screen', express.static(__dirname + '/../screen'));
+app.use('/control', express.static(__dirname + '/../control'));
+
+io.on('connection', function (socket) {
+  console.log("New socket connection");
+
+  socket.on('reload', function () {
+    socket.broadcast.emit('reload');
+  });
+
+  socket.on('disconnect', function () {
+  });
+});
+
+function ab2str(buf) {
+  return String.fromCharCode.apply(null, new Uint16Array(buf));
+}
+
+arduinoSerial.on("open", function () {
+  console.log('Opening serial port');
+
+  arduinoSerial.on('data', function(data) {
+    console.log('data received: ' + data);
+    io.emit('t', ab2str(data));
+  });
+
+});
+
