63326f6a14f0ff8e918f574577f81836daec48b8
Michael Dewberry <michael.dewberry@gmail>
2015-07-26 01:22:45 -0400

    Clean up flow, autodetect Arduino port name (on OSX)

--- Display/server/main.js
+++ Display/server/main.js
@@ -1,16 +1,9 @@
+// Express serves screen and control interfaces
+
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
-var io = require('socket.io')(server);
 var webport = process.env.PORT || 8000;
-
-var SerialPort = require("serialport").SerialPort;
-var arduinoSerial = new SerialPort("/dev/cu.usbmodem1411", {
-  baudrate: 115200
-});
-
-var fs = require("fs");
-var logFile = fs.createWriteStream("/Users/dewb/blunderwood_log", { flags: 'a' });
 
 server.listen(webport, function () {
   console.log('Server listening at port %d', webport);
@@ -19,7 +12,11 @@ server.listen(webport, function () {
 app.use('/screen', express.static(__dirname + '/../screen'));
 app.use('/control', express.static(__dirname + '/../control'));
 
-io.on('connection', function (socket) {
+// Socket.io communications with screen and control clients
+
+var socketio = require('socket.io')(server);
+
+socketio.on('connection', function (socket) {
   console.log("New socket connection");
 
   socket.on('reload', function () {
@@ -28,20 +25,42 @@ io.on('connection', function (socket) {
 
   socket.on('disconnect', function () {
   });
+  
+});
+
+// Log input to the filesystem
+
+var fs = require("fs");
+var logFile = fs.createWriteStream("/Users/dewb/blunderwood_log", { flags: 'a' });
+
+// Serial communication to Arduino or virtual serial port
+
+var serial = require("serialport");
+var arduinoPortName = "/dev/virtual-tty";
+
+serial.list(function (err, ports) {
+  ports.forEach(function(port) {
+    if (port.comName.substring(0, 16) == "/dev/cu.usbmodem") {
+      arduinoPortName = port.comName;
+    }
+  });
+});
+
+var arduinoPort = new serial.SerialPort(arduinoPortName, {
+  baudrate: 115200
 });
 
 function ab2str(buf) {
   return String.fromCharCode.apply(null, new Uint16Array(buf));
 }
 
-arduinoSerial.on("open", function () {
+arduinoPort.on("open", function () {
   console.log('Opening serial port');
 
-  arduinoSerial.on('data', function(data) {
+  arduinoPort.on('data', function(data) {
     console.log('data received: ' + data);
     logFile.write(data);
-    io.emit('t', ab2str(data));
+    socketio.emit('t', ab2str(data));
   });
-
 });
 
