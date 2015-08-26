37814a65c1f5bc8c5a7c1c9ae5cb0b8b5ae0fe9b
Michael Dewberry <michael.dewberry@gmail>
2015-08-26 15:49:11 -0400

    Refactor for server-side modes

--- Display/server/main.js
+++ Display/server/main.js
@@ -1,7 +1,4 @@
-var os = require('os');
-
 // Express serves screen and control interfaces
-
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
@@ -14,9 +11,27 @@ server.listen(webport, function () {
 app.use('/screen', express.static(__dirname + '/../screen'));
 app.use('/control', express.static(__dirname + '/../control'));
 
-// Socket.io communications with screen and control clients
-
+// Socket.io communicates with screen and control clients
 var socketio = require('socket.io')(server);
+
+// Server modes
+var SerialInputMode = require('./modes/serialInput.js');
+var serverModeList = {};
+serverModeList[SerialInputMode.name] = SerialInputMode;
+
+var currentMode = null;
+
+function setServerMode(newModeName, options) {
+  if ((currentMode == null || newModeName != currentMode.name) && newModeName in serverModeList) {
+    if (currentMode != null) {
+      currentMode.deactivate();
+    }
+    currentMode = serverModeList[newModeName];
+    currentMode.activate(socketio, options);
+  }
+}
+
+setServerMode(SerialInputMode.name, {});
 
 socketio.on('connection', function (socket) {
   console.log("New socket connection");
@@ -28,63 +43,11 @@ socketio.on('connection', function (socket) {
   socket.on('disconnect', function () {
   });
 
-});
-
-// Log input to the filesystem
-
-var fs = require("fs");
-var logFile = fs.createWriteStream("/Users/dewb/blunderwood_log", { flags: 'a' });
-
-// Serial communication to Arduino or virtual serial port
-
-var serial = require("serialport");
-var arduinoPortName = "/dev/virtual-tty";
-
-var hardwarePortPrefixes = {
-  'darwin': '/dev/cu.usbmodem',
-  'win32': 'COM',
-  'win64': 'COM',
-  'linux': '/dev/ttyACM'
-}
-
-var portPrefix = hardwarePortPrefixes[os.platform()];
-
-serial.list(function (err, ports) {
-  ports.forEach(function(port) {
-    if (port.comName.substring(0, portPrefix.length) == portPrefix) {
-      arduinoPortName = port.comName;
-    }
+  socket.on('switchServerMode', function (newModeName, options) {
+    setServerMode(newModeName, options);
   });
+
 });
 
-var arduinoPort = new serial.SerialPort(arduinoPortName, {
-  baudrate: 115200,
-  dataBits: 8,
-  parity: 'none',
-  stopBits: 1,
-  flowControl: false
-});
 
-function byteToHTML(byte) {
-  if (byte == 0x08 || byte == 0x7F) { // backspace
-    return '«';
-  } 
-
-  return String.fromCharCode(byte);
-}
-
-function ab2str(buf) {
-  return byteToHTML.apply(null, new Uint16Array(buf));
-}
-
-arduinoPort.on("open", function () {
-  console.log('Serial connection to ' + arduinoPortName + ' established');
-
-  arduinoPort.on('data', function(rawdata) {
-    var data = ab2str(rawdata);
-    console.log('data received: ' + data);
-    logFile.write(data);
-    socketio.emit('t', data);
-  });
-});
 
